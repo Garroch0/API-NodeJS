9dfe66fc9ee8dece9c8468f33e37ecd6d9a27f3d
Gustavo Garrocho <gustavobgarrocho@gmail.com>
2021-06-08 23:22:23 -0300

    Update dos relacionamentos com MongoDB

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -4,17 +4,17 @@ const authConfig = require('../../config/auth.json');
 module.exports = (req, res, next) =>{
   const authHeader = req.headers.authorization;
 
-  if(!authHeader)
+  if (!authHeader)
     return res.status(401).send({ error: 'No token provided' });
   
   const parts = authHeader.split (' ');
   
-  if(!parts.length === 2)
+  if (!parts.length === 2)
     return res.status(401).send({ error: 'Token error' });
 
   const [ scheme, token ] = parts;
 
-  if(!/^Bearer$/i.test(scheme))
+  if (!/^Bearer$/i.test(scheme))
     return res.status(401).send({ error: 'Token malformatted'});
 
   jwt.verify(token, authConfig.secret, (err, decoded)=> {
